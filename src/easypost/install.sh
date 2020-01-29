89a9922cd39812a1d8f6f763aec8056cbb0412cb
Justintime50 <justinpaulhammond@gmail.com>
2020-01-28 18:04:55 -0700

    Added personal dotfiles, automated installs

--- src/easypost/install.sh
+++ src/easypost/install.sh
@@ -1,5 +1,9 @@
 #/!bin/bash
 
 echo "Installing EasyPost dotfiles..."
-cp -a src/easypost/. $HOME
+mkdir "$HOME"/temp && cd "$HOME"/temp || exit
+git clone https://github.com/Justintime50/dotfiles.git > /dev/null 2>&1
+cp -a "$HOME"/temp/dotfiles/src/easypost/. "$HOME"
+rm -rf "$HOME"/temp
+rm "$HOME/install.sh"
 echo "EasyPost dotfiles installed!"
