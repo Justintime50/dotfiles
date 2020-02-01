e4586fb51718c137b8fab09793efcbd9d066460e
Justintime50 <justinpaulhammond@gmail.com>
2020-02-01 13:58:54 -0700

    Updated dotfiles temp foldername to avoid conflict

--- src/easypost/install.sh
+++ src/easypost/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Installing EasyPost dotfiles..."
-mkdir "$HOME"/temp && cd "$HOME"/temp || exit
+mkdir "$HOME"/dotfilestemp && cd "$HOME"/dotfilestemp || exit
 git clone https://github.com/Justintime50/dotfiles.git > /dev/null 2>&1
-cp -a "$HOME"/temp/dotfiles/src/easypost/. "$HOME"
-rm -rf "$HOME"/temp
+cp -a "$HOME"/dotfilestemp/dotfiles/src/easypost/. "$HOME"
+rm -rf "$HOME"/dotfilestemp
 rm "$HOME/install.sh"
 echo "EasyPost dotfiles installed!"
