54a9778499ef504c3fa1a6b6a06328d5c35766d4
Justintime50 <justinpaulhammond@gmail.com>
2020-02-01 13:58:54 -0700

    Updated dotfiles temp foldername to avoid conflict

--- src/personal/install.sh
+++ src/personal/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Installing personal dotfiles..."
-mkdir "$HOME"/temp && cd "$HOME"/temp || exit
+mkdir "$HOME"/dotfilestemp && cd "$HOME"/dotfilestemp || exit
 git clone https://github.com/Justintime50/dotfiles.git > /dev/null 2>&1
-cp -a "$HOME"/temp/dotfiles/src/personal/. "$HOME"
-rm -rf "$HOME"/temp
+cp -a "$HOME"/dotfilestemp/dotfiles/src/personal/. "$HOME"
+rm -rf "$HOME"/dotfilestemp
 rm "$HOME/install.sh"
 echo "Personal dotfiles installed!"
