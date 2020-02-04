651315d36dd3ebe5cbd7ab733f019bdf3396b32c
Justintime50 <justinpaulhammond@gmail.com>
2020-02-04 14:09:32 -0700

    Fix travis

--- src/easypost/install.sh
+++ src/easypost/install.sh
@@ -6,5 +6,6 @@ git clone https://github.com/Justintime50/dotfiles.git > /dev/null 2>&1
 cp -a "$HOME"/dotfilestemp/dotfiles/src/easypost/. "$HOME"
 rm -rf "$HOME"/dotfilestemp
 rm "$HOME/install.sh"
-source ~/.zshrc
+# shellcheck disable=SC1090
+source "$HOME"/.zshrc
 echo "EasyPost dotfiles installed!"
