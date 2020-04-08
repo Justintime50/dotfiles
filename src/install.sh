0ee40e2550397940c57f8bb2ee1052b1de528e92
Justintime50 <justinpaulhammond@gmail.com>
2020-04-08 17:05:50 -0600

    Updated install script to not cd to dir

--- src/install.sh
+++ src/install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 echo "Installing dotfiles..."
-cd "$HOME" || exit
-git clone https://github.com/Justintime50/dotfiles.git > /dev/null 2>&1
+git clone https://github.com/Justintime50/dotfiles.git "$HOME" > /dev/null 2>&1
 cp -a "$HOME"/dotfiles/src/home/. "$HOME" # copy home dir files
 # sudo cp -a "$HOME"/dotfiles/src/hosts /etc/hosts # copy hosts file
 rm -rf "$HOME"/dotfiles
 # shellcheck disable=SC1090
-source ~/.zshrc
+source "$HOME"/.zshrc
 echo "Dotfiles installed!"
