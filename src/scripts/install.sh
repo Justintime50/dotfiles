fbcb908c72d469e42c24d1eb7d352a0f4e9304c1
Justintime50 <justinpaulhammond@gmail.com>
2020-05-28 17:09:56 -0600

    Cleanup to install script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -12,11 +12,9 @@ else
     cd || exit
 fi
 
-
 # Install dotfiles
 cp -a "$HOME"/dotfiles/src/home/. "$HOME" # copy home dir files
 # sudo cp -a "$HOME"/dotfiles/src/hosts /etc/hosts # copy hosts file
-# rm -rf "$HOME"/dotfiles
 
 # Cleanup
 source "$HOME"/.zshrc
