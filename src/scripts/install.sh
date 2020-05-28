0cd5813ce7b5117c1f74c7b4917c9d08069bc94c
Justintime50 <justinpaulhammond@gmail.com>
2020-05-28 17:07:09 -0600

    Updates to install script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -6,7 +6,12 @@ echo "Installing dotfiles..."
 # Check if dotfiles project exists and clone if not
 if [[ ! -d "$HOME"/dotfiles ]] ; then
     git clone https://github.com/Justintime50/dotfiles.git "$HOME"/dotfiles > /dev/null 2>&1
+else
+    cd "$HOME"/dotfiles || exit
+    git pull
+    cd || exit
 fi
+
 
 # Install dotfiles
 cp -a "$HOME"/dotfiles/src/home/. "$HOME" # copy home dir files
