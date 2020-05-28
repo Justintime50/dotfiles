b8b7e94defbe7f8fd11243507323820098c1d8d3
Justintime50 <justinpaulhammond@gmail.com>
2020-05-28 17:02:05 -0600

    Fixed dotfile installation

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -5,7 +5,7 @@ echo "Installing dotfiles..."
 
 # Check if dotfiles project exists and clone if not
 if [[ ! -d "$HOME"/dotfiles ]] ; then
-    git clone https://github.com/Justintime50/dotfiles.git "$HOME" > /dev/null 2>&1
+    git clone https://github.com/Justintime50/dotfiles.git "$HOME"/dotfiles > /dev/null 2>&1
 fi
 
 # Install dotfiles
