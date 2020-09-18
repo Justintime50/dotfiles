543b0e0727800e4be529021632fd164476ec4a85
Justintime50 <justinpaulhammond@gmail.com>
2020-09-18 15:06:08 -0600

    Fixing script auto-exiting

--- src/core/functions.sh
+++ src/core/functions.sh
@@ -10,15 +10,15 @@ cat "$HOME"/dotfiles/src/core/loaded-modules.txt
 # check that dotfiles are up to date each time a terminal starts
 echo ""
 echo "Dotfiles status: "
-git -C "$HOME"/dotfiles remote update > /dev/null || echo "Error updating from remote Dotfiles"; exit 1
-git -C "$HOME"/dotfiles status -s -b || echo "Couldn't check remote Dotfiles"; exit 1
+git -C "$HOME"/dotfiles remote update > /dev/null || { echo "Error updating from remote Dotfiles"; exit 1; }
+git -C "$HOME"/dotfiles status -s -b || { echo "Couldn't check remote Dotfiles"; exit 1; }
 echo "##################################################"
 
 ## Dotfile Functions ##
 function push-dotfiles {
 	git -C "$HOME"/dotfiles add .
 	git -C "$HOME"/dotfiles commit -m "Updated dotfiles"
-	git -C "$HOME"/dotfiles push > /dev/null || echo "Error pushing Dotfiles"; exit 1
+	git -C "$HOME"/dotfiles push > /dev/null || { echo "Error pushing Dotfiles"; exit 1; }
 	echo "Dotfiles pushed!"
 }
 function pull-dotfiles {
