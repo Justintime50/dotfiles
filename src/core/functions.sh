44e087a53af0d2ad6e34228708a7aa452522b692
Justintime50 <justinpaulhammond@gmail.com>
2020-09-21 15:34:16 -0600

    Code reuse through variables, added clean-dotfiles function, wrapped up others in functions

--- src/core/functions.sh
+++ src/core/functions.sh
@@ -1,26 +1,40 @@
 # shellcheck disable=SC1090,SC2148
 
-USER=Justintime50
+DOTFILES_DIR="$HOME/dotfiles"
+GITHUB_USER=Justintime50
 
-# Print dotfiles message on shell start
-echo "#################### $SHELL ####################"
-echo "Powered by $USER's Dotfiles"
-echo "Loaded modules:"
-cat "$HOME"/dotfiles/src/core/loaded-modules.txt
-# check that dotfiles are up to date each time a terminal starts
-echo ""
-echo "Dotfiles status: "
-git -C "$HOME"/dotfiles remote update > /dev/null 2>&1 || { echo "Error updating from remote Dotfiles"; exit 1; }
-git -C "$HOME"/dotfiles status -s -b || { echo "Couldn't check remote Dotfiles"; exit 1; }
-echo "##################################################"
+function print_dotfiles_message {
+	# Print dotfiles message on each shell start
+	echo "#################### $SHELL ####################"
+	echo "Powered by $GITHUB_USER's Dotfiles"
+	echo "Loaded modules:"
+	cat "$DOTFILES_DIR"/src/core/loaded-modules.txt
+	# check that dotfiles are up to date each time a terminal starts
+	echo ""
+	echo "Dotfiles status: "
+	git -C "$DOTFILES_DIR" remote update > /dev/null 2>&1 || { echo "Error updating from remote Dotfiles"; exit 1; }
+	git -C "$DOTFILES_DIR" status -s -b || { echo "Couldn't check remote Dotfiles"; exit 1; }
+	echo "##################################################"
+}
+print_dotfiles_message
 
 ## Dotfile Functions ##
 function push-dotfiles {
-	git -C "$HOME"/dotfiles add .
-	git -C "$HOME"/dotfiles commit -m "Updated dotfiles"
-	git -C "$HOME"/dotfiles push > /dev/null 2>&1 || { echo "Error pushing Dotfiles"; exit 1; }
+	git -C "$DOTFILES_DIR" add .
+	git -C "$DOTFILES_DIR" commit -m "Updated dotfiles"
+	git -C "$DOTFILES_DIR" push > /dev/null 2>&1 || { echo "Error pushing Dotfiles"; exit 1; }
 	echo "Dotfiles pushed!"
 }
 function pull-dotfiles {
 	bash <(curl -s https://raw.githubusercontent.com/justintime50/dotfiles/master/src/scripts/install.sh)
+}
+function clean-dotfiles {
+	# Remove symlinks and restore original core .zshrc config
+	ln -sfn "$DOTFILES_DIR"/src/core/.zshrc "$HOME"/.zshrc
+	rm -i "$HOME"/.gitconfig
+	# rm -i /etc/hosts
+	rm -i "$HOME"/.gitconfig-easypost
+    rm -i "$HOME"/.ssh/config
+	rm -i "$HOME"/.emacs
+	echo "Dotfiles cleaned!"
 }
--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,67 +1,94 @@
 #!/bin/bash
 # shellcheck disable=SC1090
 
-# Check if dotfiles project exists and clone if not
-echo "Installing dotfiles..."
-if [[ ! -d "$HOME"/dotfiles ]] ; then
-    git clone https://github.com/Justintime50/dotfiles.git "$HOME"/dotfiles > /dev/null 2>&1 || { echo "Error cloning Dotfiles"; exit 1; }
-else
-    git -C "$HOME"/dotfiles pull > /dev/null 2>&1 || { echo "Error pulling Dotfiles"; exit 1; }
-fi
+DOTFILES_DIR="$HOME/dotfiles"
 
-# Install dotfiles core
-cp "$HOME"/dotfiles/src/core/.zshrc "$HOME"/.zshrc # Install the global "~/.zshrc which will source the rest"
-LOADED_MODULES_FILE="$HOME/dotfiles/src/core/loaded-modules.txt"
-[ -f "$LOADED_MODULES_FILE" ] && rm "$LOADED_MODULES_FILE"
-echo "* Core" >> "$LOADED_MODULES_FILE"
+function install_core {
+    # Pull the dotfiles project if it exists, clone if it does not
+    echo "Installing dotfiles..."
+    if [[ ! -d "$DOTFILES_DIR" ]] ; then
+        git clone https://github.com/Justintime50/dotfiles.git "$DOTFILES_DIR" > /dev/null 2>&1 || { echo "Error cloning Dotfiles"; exit 1; }
+    else
+        git -C "$DOTFILES_DIR" pull > /dev/null 2>&1 || { echo "Error pulling Dotfiles"; exit 1; }
+    fi
 
-# Iterate through each set of dotfiles prompting which to install
-echo "Install personal dotfiles? (y/N)"
-read -r PERSONAL
-case $PERSONAL in
-    y|Y ) echo "Installing personal dotfiles..." ;;
-    n|N ) echo "Skipping personal dotfiles." ;;
-    * ) echo "Y or N input required"; exit 1 ;;
-esac
+    # Install dotfiles core
+    ln -sfn "$DOTFILES_DIR"/src/core/.zshrc "$HOME"/.zshrc # Install the global "~/.zshrc which will source the rest"
+    LOADED_MODULES_FILE="$DOTFILES_DIR/src/core/loaded-modules.txt"
+    [ -f "$LOADED_MODULES_FILE" ] && rm "$LOADED_MODULES_FILE"
+    echo "* Core" >> "$LOADED_MODULES_FILE"
+}
 
-echo "Install EasyPost dotfiles? (y/N)"
-read -r EASYPOST
-case $EASYPOST in
-    y|Y ) echo "Installing EasyPost dotfiles..." ;;
-    n|N ) echo "Skipping EasyPost dotfiles." ;;
-    * ) echo "Y or N input required"; exit 1 ;;
-esac
+function prompt_dotfile_modules {
+    # Iterate through each set of dotfiles prompting which to install
+    echo "Install personal dotfiles? (y/N)"
+    read -r PERSONAL
+    case $PERSONAL in
+        y|Y ) echo "Installing personal dotfiles..." ;;
+        n|N ) echo "Skipping personal dotfiles." ;;
+        * ) echo "Y or N input required"; exit 1 ;;
+    esac
 
-echo "Install Emacs dotfiles? (y/N)"
-read -r EMACS
-case $EMACS in
-    y|Y ) echo "Installing Emacs dotfiles..." ;;
-    n|N ) echo "Skipping Emacs dotfiles." ;;
-    * ) echo "Y or N input required"; exit 1 ;;
-esac
+    echo "Install EasyPost dotfiles? (y/N)"
+    read -r EASYPOST
+    case $EASYPOST in
+        y|Y ) echo "Installing EasyPost dotfiles..." ;;
+        n|N ) echo "Skipping EasyPost dotfiles." ;;
+        * ) echo "Y or N input required"; exit 1 ;;
+    esac
 
-# Based on input, install dotfiles
-if [[ $PERSONAL = "y" || $PERSONAL = "Y" ]] ; then
-    ln -sfn "$HOME"/dotfiles/src/personal/home/.gitconfig "$HOME"/.gitconfig
-    # ln -sfn "$HOME"/dotfiles/src/personal/misc/hosts /etc/hosts
-    echo "source $HOME/dotfiles/src/personal/home/.zshrc" >> "$HOME"/.zshrc
-    echo "* Personal" >> "$LOADED_MODULES_FILE"
-    echo "Personal dotfiles installed!"
-fi
+    echo "Install EasyPost AWS dotfiles? (y/N)"
+    read -r EASYPOST_AWS
+    case $EASYPOST_AWS in
+        y|Y ) echo "Installing EasyPost AWS dotfiles..." ;;
+        n|N ) echo "Skipping EasyPost AWS dotfiles." ;;
+        * ) echo "Y or N input required"; exit 1 ;;
+    esac
 
-if [[ $EASYPOST = "y" || $EASYPOST = "Y" ]] ; then
-    ln -sfn "$HOME"/dotfiles/src/easypost/.gitconfig-easypost "$HOME"/.gitconfig-easypost
-    ln -sfn "$HOME"/dotfiles/src/easypost/ssh/config "$HOME"/.ssh/config
-    echo "source $HOME/dotfiles/src/easypost/.zshrc" >> "$HOME"/.zshrc
-    echo "* EasyPost" >> "$LOADED_MODULES_FILE"        
-    echo "EasyPost dotfiles installed!"
-fi
+    echo "Install Emacs dotfiles? (y/N)"
+    read -r EMACS
+    case $EMACS in
+        y|Y ) echo "Installing Emacs dotfiles..." ;;
+        n|N ) echo "Skipping Emacs dotfiles." ;;
+        * ) echo "Y or N input required"; exit 1 ;;
+    esac
+}
 
-if [[ $EMACS = "y" || $EMACS = "Y" ]] ; then
-    ln -sfn "$HOME"/dotfiles/src/emacs/.emacs "$HOME"/.emacs
-    echo "* Emacs" >> "$LOADED_MODULES_FILE"
-    echo "EasyPost dotfiles installed!"
-fi
+function install_modules {
+    # Based on input, install dotfiles
+    if [[ $PERSONAL = "y" || $PERSONAL = "Y" ]] ; then
+        ln -sfn "$DOTFILES_DIR"/src/personal/home/.gitconfig "$HOME"/.gitconfig
+        # ln -sfn "$DOTFILES_DIR"/src/personal/misc/hosts /etc/hosts
+        echo "source $DOTFILES_DIR/src/personal/home/.zshrc" >> "$HOME"/.zshrc
+        echo "* Personal" >> "$LOADED_MODULES_FILE"
+        echo "Personal dotfiles installed!"
+    fi
 
-# Source core files
+    if [[ $EASYPOST = "y" || $EASYPOST = "Y" ]] ; then
+        ln -sfn "$DOTFILES_DIR"/src/easypost/.gitconfig-easypost "$HOME"/.gitconfig-easypost
+        ln -sfn "$DOTFILES_DIR"/src/easypost/ssh/config "$HOME"/.ssh/config
+        echo "source $DOTFILES_DIR/src/easypost/.zshrc" >> "$HOME"/.zshrc
+        echo "* EasyPost" >> "$LOADED_MODULES_FILE"        
+        echo "EasyPost dotfiles installed!"
+    fi
+
+    if [[ $EASYPOST_AWS = "y" || $EASYPOST_AWS = "Y" ]] ; then
+        ln -sfn "$DOTFILES_DIR"/src/easypost/.gitconfig-easypost-aws "$HOME"/.gitconfig
+        echo "* EasyPost AWS" >> "$LOADED_MODULES_FILE"        
+        echo "EasyPost AWS dotfiles installed!"
+    fi
+
+    if [[ $EMACS = "y" || $EMACS = "Y" ]] ; then
+        ln -sfn "$DOTFILES_DIR"/src/emacs/.emacs "$HOME"/.emacs
+        echo "* Emacs" >> "$LOADED_MODULES_FILE"
+        echo "Emacs dotfiles installed!"
+    fi
+}
+
+# Run the installation script functions
+install_core
+prompt_dotfile_modules
+install_modules
+
+# Source Dotfiles after installation
 source "$HOME"/.zshrc
