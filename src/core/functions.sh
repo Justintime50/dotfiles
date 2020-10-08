1e321fa63d1b6a9fc4a483c4f0ae88f70d3ad491
Justintime50 <justinpaulhammond@gmail.com>
2020-10-07 23:15:00 -0600

    Patching additional bugs in the install script

--- src/core/functions.sh
+++ src/core/functions.sh
@@ -3,7 +3,7 @@
 DOTFILES_DIR="$HOME/dotfiles"
 GITHUB_USER=Justintime50
 
-function print_dotfiles_message {
+print_dotfiles_message() {
 	# Print dotfiles message on each shell start
 	echo "#################### $SHELL ####################"
 	echo "Powered by $GITHUB_USER's Dotfiles"
@@ -16,32 +16,41 @@ function print_dotfiles_message {
 	git -C "$DOTFILES_DIR" status -s -b || { echo "Couldn't check remote Dotfiles"; exit 1; }
 	echo "##################################################"
 }
-print_dotfiles_message
 
 ## Dotfile Functions ##
-function push-dotfiles {
+push-dotfiles() {
 	git -C "$DOTFILES_DIR" add .
 	git -C "$DOTFILES_DIR" commit -m "Updated dotfiles"
 	git -C "$DOTFILES_DIR" push > /dev/null 2>&1 || { echo "Error pushing Dotfiles"; exit 1; }
 	echo "Dotfiles pushed!"
 }
 
-function pull-dotfiles {
+pull-dotfiles() {
 	bash <(curl -s https://raw.githubusercontent.com/justintime50/dotfiles/master/src/scripts/install.sh)
 }
 
-function clean-dotfiles {
+clean-dotfiles() {
 	# Remove symlinks and restore original core .zshrc config
+	
 	# Core
 	cp "$DOTFILES_DIR"/src/core/.zshrc "$HOME"/.zshrc
+	
 	# Personal
 	rm -i "$HOME"/.gitconfig
+	rm -i "$HOME"/.zlogin
 	# rm -i /etc/hosts
+	
 	# EasyPost
 	rm -i "$HOME"/.gitconfig-easypost
     rm -i -rf "$HOME"/.ssh/config
+
+	# EasyPost AWS
+	rm -i 	rm -i "$HOME"/.gitconfig-easypost-aws
+	
 	# Emacs
 	rm -i "$HOME"/.emacs
 
 	echo "Dotfiles cleaned!"
 }
+
+print_dotfiles_message
