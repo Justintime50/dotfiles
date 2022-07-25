6f466a97b7dbba35476d6ef7cacc007a3ea45eba
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-07-25 11:50:11 -0600

    Updated dotfiles

--- dots-config.sh
+++ dots-config.sh
@@ -10,7 +10,9 @@ dots_config_up() {
     if [[ "$HOSTNAME" == "$PERSONAL_HOSTNAME" ]]; then
         ln -sfn "$DOTFILES_DIR"/src/personal/home/.gitconfig "$HOME"/.gitconfig
         ln -sfn "$DOTFILES_DIR"/src/personal/home/.zlogin "$HOME"/.zlogin
-        ln -sfn "$DOTFILES_DIR"/src/emacs/.emacs "$HOME"/.emacs
+	ln -sfn "$DOTFILES_DIR"/src/emacs/.emacs "$HOME"/.emacs
+        ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.vimrc
+	ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.config/nvim/init.vim
         crontab - <"$DOTFILES_DIR"/src/personal/crontab
         echo ". $DOTFILES_DIR/src/personal/home/.zshrc" >>"$HOME"/.zshrc
     elif [[ "$HOSTNAME" == "$EASYPOST_HOSTNAME" ]]; then
@@ -18,17 +20,23 @@ dots_config_up() {
         ln -sfn "$DOTFILES_DIR"/src/personal/home/.zlogin "$HOME"/.zlogin
         ln -sfn "$DOTFILES_DIR"/src/easypost/ssh/config "$HOME"/.ssh/config
         ln -sfn "$DOTFILES_DIR"/src/emacs/.emacs "$HOME"/.emacs
+        ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.vimrc
+	ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.config/nvim/init.vim
         crontab - <"$DOTFILES_DIR"/src/easypost/crontab
         echo ". $DOTFILES_DIR/src/personal/home/.zshrc" >>"$HOME"/.zshrc
         echo ". $DOTFILES_DIR/src/easypost/.zshrc" >>"$HOME"/.zshrc
     elif [[ "$HOSTNAME" == "$EASYPOST_AWS_HOSTNAME" ]]; then
         ln -sfn "$DOTFILES_DIR"/src/easypost/.gitconfig-easypost-aws "$HOME"/.gitconfig
         ln -sfn "$DOTFILES_DIR"/src/emacs/.emacs "$HOME"/.emacs
+        ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.vimrc
+	ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.config/nvim/init.vim
         echo ". $DOTFILES_DIR/src/easypost/.zshrc-aws" >>"$HOME"/.zshrc
     elif [[ "$HOSTNAME" == *"$SERVER_HOSTNAME"* ]]; then
         ln -sfn "$DOTFILES_DIR"/src/personal/home/.gitconfig "$HOME"/.gitconfig
         ln -sfn "$DOTFILES_DIR"/src/personal/home/.zlogin "$HOME"/.zlogin
         ln -sfn "$DOTFILES_DIR"/src/emacs/.emacs "$HOME"/.emacs
+        ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.vimrc
+	ln -sfn "$DOTFILES_DIR"/src/vim/.vimrc "$HOME"/.config/nvim/init.vim
         echo ". $DOTFILES_DIR/src/personal/home/.zshrc" >>"$HOME"/.zshrc
         if [[ "$HOSTNAME" == "$SERVER2_HOSTNAME" ]]; then
             crontab - <"$DOTFILES_DIR"/src/server/crontab
@@ -44,20 +52,28 @@ dots_config_down() {
         rm "$HOME"/.gitconfig
         rm "$HOME"/.zlogin
         rm "$HOME"/.emacs
+	rm "$HOME"/.vimrc
+	rm "$HOME"/.config/nvim/init.vim
         crontab -r
     elif [[ "$HOSTNAME" == "$EASYPOST_HOSTNAME" ]]; then
         rm "$HOME"/.gitconfig
         rm "$HOME"/.zlogin
         rm -rf "$HOME"/.ssh/config
         rm "$HOME"/.emacs
-        crontab -r
+       	rm "$HOME"/.vimrc
+	rm "$HOME"/.config/nvim/init.vim
+	 crontab -r
     elif [[ "$HOSTNAME" == "$EASYPOST_AWS_HOSTNAME" ]]; then
         rm "$HOME"/.gitconfig
         rm "$HOME"/.emacs
-    elif [[ "$HOSTNAME" == *"$SERVER_HOSTNAME"* ]]; then
+ 	rm "$HOME"/.vimrc
+	rm "$HOME"/.config/nvim/init.vim
+   elif [[ "$HOSTNAME" == *"$SERVER_HOSTNAME"* ]]; then
         rm "$HOME"/.gitconfig
         rm "$HOME"/.zlogin
         rm "$HOME"/.emacs
+  	rm "$HOME"/.vimrc
+	rm "$HOME"/.config/nvim/init.vim
         crontab -r
     else
         echo "HOSTNAME doesn't match any config."
---
+++ src/vim/.vimrc
@@ -0,0 +1,49 @@
+" Vim Config
+if v:progname =~? "evim"
+  finish
+endif
+
+" Get the defaults that most users want.
+" This may not be compatible with Neovim
+" source $VIMRUNTIME/defaults.vim
+
+if has("vms")
+  set nobackup		" do not keep a backup file, use versions instead
+else
+  set backup		" keep a backup file (restore to previous version)
+  if has('persistent_undo')
+    set undofile	" keep an undo file (undo changes after closing)
+  endif
+endif
+
+" The following settings only take effect when Vim is running on a GUI that supports colors
+if &t_Co > 2 || has("gui_running")
+  " Switch on highlighting the last used search pattern.
+  set hlsearch
+endif
+
+" Setup search settings
+set ignorecase
+set incsearch
+
+" Setup line numbers
+set number
+set relativenumber
+
+" Put these in an autocmd group, so that we can delete them easily.
+augroup vimrcEx
+  au!
+
+  " For all text files set 'textwidth' to 120 characters.
+  autocmd FileType text setlocal textwidth=120
+augroup END
+
+" Add optional packages.
+"
+" The matchit plugin makes the % command work better, but it is not backwards
+" compatible.
+" The ! means the package won't be loaded right away but when plugins are
+" loaded during initialization.
+if has('syntax') && has('eval')
+  packadd! matchit
+endif
