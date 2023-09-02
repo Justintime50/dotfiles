4a0cffff87dc3f0caea98d426b9443b33b258377
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-09-02 09:20:39 -0600

    Updated dotfiles

--- src/server/login-commands.sh
+++ src/server/login-commands.sh
@@ -3,5 +3,8 @@
 # Wait for the system and services to boot before attempting to do anything
 sleep 30
 
+# Ensure SSH Agent is loaded and authenticated (SSH is needed for various cron tasks)
+ssh-add -l
+
 # Start Harvey
 cd "$HOME"/git/personal/harvey && just prod
